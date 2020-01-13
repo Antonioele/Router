1e550cc81270ea235f6dfdb25e8db5320d43d20c
Antonio Aiello <antonioele@tiscali.it>
2020-01-13 15:57:13 +0100

    add router 100  Cycleiterator

---
+++ Console/Command/GreetingCommand.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Antonioele\Router\Console\Command;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class GreetingCommand
+ */
+class GreetingCommand extends Command
+{
+    /**
+     * Name argument
+     */
+    const NAME_ARGUMENT = 'name';
+
+    /**
+     * Allow option
+     */
+    const ALLOW_ANONYMOUS = 'allow-anonymous';
+
+    /**
+     * Anonymous name
+     */
+    const ANONYMOUS_NAME = 'Anonymous';
+
+    /**
+     * {@inheritdoc}
+     *
+     */
+    private $productRepository;
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+        parent::__construct();
+
+    }
+    protected function configure()
+    {
+        $this->setName('example:greeting')
+            ->setDescription('Greeting command')
+            ->setDefinition([
+                new InputArgument(
+                    self::NAME_ARGUMENT,
+                    InputArgument::OPTIONAL,
+                    'Name'
+                ),
+                new InputOption(
+                    self::ALLOW_ANONYMOUS,
+                    '-a',
+                    InputOption::VALUE_NONE,
+                    'Allow anonymous'
+                ),
+
+            ]);
+
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $product = $this->productRepository->getById(1402);
+        $name = $input->getArgument(self::NAME_ARGUMENT);
+        $allowAnonymous = $input->getOption(self::ALLOW_ANONYMOUS);
+        if (is_null($name)) {
+            if ($allowAnonymous) {
+                $name = self::ANONYMOUS_NAME;
+            } else {
+                throw new \InvalidArgumentException('Argument ' . self::NAME_ARGUMENT . ' is missing.');
+            }
+        }
+        $output->writeln('<info>Hello ' . $name . '!</info>'.$product->getName());
+    }
+}
